b5f074a9175caa8d54f2d200487d9a8f1a4ba94a
gabrielcggs <gabrielcggsilva@gmail.com>
2026-04-24 09:35:55 -0300

    fim do menu

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h> 
 
 void menu(void){   
-    printf("---MENU---\n");
-    printf("1 - CALCULADORA DE MEDIA;\n");
-    printf("Selecione uma das opcoes acima: ");}
+    printf("==============================\n");
+    printf("SISTEMA EQUIPE XYZ\n");
+    printf("==============================\n");
+    printf("1 - Inserir notas\n");
+    printf("2 - Calcular media\n");
+    printf("3 - Verificar situacao\n");
+    printf("4 - Exibir resultado\n");
+    printf("5 - Calcular derivada\n");
+    printf("6 - Sair\n");
+    printf("Escolha uma opcao: ");
+
+}
 
 int main(){
 
 
     
-    
+    menu();
 
     
      
