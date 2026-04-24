553426a61800ebd5e7032f89f307b09f85bd1530
carvalho_07 <GabrielFIFA777@gmail.com>
2026-04-24 10:00:06 -0300

    Calculando a media

--- main.c
+++ main.c
@@ -15,15 +15,32 @@ void menu(void){
 }
 
 int main(){
-int opcao;
-float nota_1, nota_2, media;
+    int opcao;
+    float nota_1, nota_2, media;
+
 
     
     menu();
+    scanf("%d", &opcao);
+    switch(opcao){
+    case 1: 
+    printf("--- CALCULADORA DE MEDIA ---\n");
+    printf("Digite sua primeira nota: ");
+    scanf("%f", &nota_1);
+    printf("Digite sua segunda nota: ");
+    scanf("%f", &nota_2);
+    if(nota_1 < 11 && nota_1 >= 0 && nota_2 < 11 && nota_2 >= 0){
+        media = (nota_1 + nota_2)/2;
+       printf("A media do aluno e: %.2f. O calculo para o valor da media e (%.2f+ %.2f)/2", media, nota_1, nota_2);
+
+    }else{
+        printf("Notas fora do limite apresentadas!");
+    }
+    break;
 
     
      
 
-
+    }
     return 0;
 }
