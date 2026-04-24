d85da054688f0ea15a77cfa5fe1db9009f31683f
Gcedro <gcedro0408@outlook.com>
2026-04-24 09:13:32 -0300

    fiz o void

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+void menu(void){   
+    printf("---MENU---\n");
+    printf("1 - CALCULADORA DE MEDIA;\n");
+    printf("Selecione uma das opcoes acima: ");}
+
+int main(){
+
+
+    
+    
+
+    
+     
+
+
+    return 0;
+}
