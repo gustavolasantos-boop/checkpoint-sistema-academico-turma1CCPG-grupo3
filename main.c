d485f2bc135f4fc8a3343257bf7f2c9bbeefa502
Gustavo Lima Andrade Santos <gustavolasantos@gmail.com>
2026-04-24 11:18:21 -0300

    correção do case 4

--- main.c
+++ main.c
@@ -23,15 +23,15 @@ int main(){
     menu();
     scanf("%d", &opcao);
     switch(opcao){
+
     case 1: 
-    printf("--- CALCULADORA DE MEDIA ---\n");
+
     printf("Digite sua primeira nota: ");
     scanf("%f", &nota_1);
     printf("Digite sua segunda nota: ");
     scanf("%f", &nota_2);
     if(nota_1 < 11 && nota_1 >= 0 && nota_2 < 11 && nota_2 >= 0){
-        media = (nota_1 + nota_2)/2;
-       printf("A media do aluno e: %.2f. O calculo para o valor da media e (%.2f+ %.2f)/2", media, nota_1, nota_2);
+        
 
     }else{
         printf("Notas fora do limite apresentadas!");
@@ -43,10 +43,19 @@ int main(){
     printf(" media calculada:%.2", media);
     break;
 
-    
-
-    
+    case 3:
+    if(media >= 7){
+    printf("Aprovado\n");
+    }else{
+    printf("Reprovado\n");
+    }
+    break;
      
+    case 4:
+        printf("Nota1: %.2f\n", nota_1);
+        printf("Nota2: %.2f\n", nota_2);
+        printf("Media: %.2f\n", media);
+        break;
 
     }
     return 0;
